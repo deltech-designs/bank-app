a911a96a9b844119b1fd7d7af80833f8b8f0b18f
deltechdesigns <deltechdesigns@gmail.com>
2025-04-16 16:07:12 +0100

    first commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,51 @@
-document.querySelector("#menuToggle").addEventListener("click", function () {
-  document.querySelector(".toggleMenu").classList.toggle("hidden");
+// document.querySelector('#menuToggle').addEventListener('click', function () {
+//   document.querySelector('.toggleMenu').classList.toggle('hidden');
+// });
+
+// document.getElementById('requestBtn').addEventListener('click', () => {
+//   window.location.href = 'login.html';
+// });
+
+// && || !
+
+const user = [];
+
+const getUser = () => {
+  const user = JSON.parse(localStorage.getItem('users')) || [];
+  return user ? JSON.parse(user) : null;
+};
+
+function setUser(user) {
+  localStorage.setItem('users', JSON.stringify(user));
+}
+
+document.querySelector('#signup_button').addEventListener('click', () => {
+  const fullName = document.querySelector('#full_name').value;
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  if (!fullName || !email || !password) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  const userData = {
+    fullName,
+    email,
+    password,
+    balance: 0,
+    transactions: [],
+    saves: 0,
+    id: Date.now(),
+    createdAt: new Date().toLocaleString(),
+  };
+
+  user.push(userData);
+  setUser(user);
+
+  alert('Account created successfully!');
+  window.location.href = 'login.html';
 });
 
-document.getElementById("requestBtn").addEventListener("click", () => {
-  window.location.href = "form.html";
-});
+
+
